4ce7c076d989d4aa3f2e1d8bb390f69cafc7f644
pitt583 <3026457891@qq.com>
2026-09-13 21:14:08 +0800

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# import pandas as pd
+# df = pd.DataFrame({'姓名':['小姜','小王','小恒','小张'],
+#                 '性别':['男','男','男','男'],
+#                 '年龄':['19','19','18','19'],
+#                 '爱好':['篮球','篮球','篮球','篮球']})
+# df.set_index('姓名',inplace=True)
+# print(df)
+print("hello world")
